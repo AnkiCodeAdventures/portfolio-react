0ec74dc9eceee38a4f9eae463440af5bb21cc0be
Ankita Agarwal <ankitaag94@gmail.com>
2023-12-18 20:04:06 +0530

    make hero section responsive

--- src/containers/Hero/Hero.jsx
+++ src/containers/Hero/Hero.jsx
@@ -3,7 +3,7 @@ import styles from "./hero.module.css";
 
 function Hero() {
   return (
-    <div>
+    <div className={styles.heroContainer}>
       <section className={styles.hero}>
         <Nav />
         <div className={styles.heroText}>
