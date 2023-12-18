1b380a06c0bc513127e2dead954039f199c6b59b
Ankita Agarwal <ankitaag94@gmail.com>
2023-12-18 13:43:11 +0530

    add hero section

--- src/containers/Hero/Hero.jsx
+++ src/containers/Hero/Hero.jsx
@@ -6,6 +6,18 @@ function Hero() {
     <div>
       <section className={styles.hero}>
         <Nav />
+        <div className={styles.heroText}>
+          <p>
+            Hi, my name is
+            <span>Ankita Agarwal</span>
+          </p>
+          <h2>I develop the frontend of websites</h2>
+          <p>
+            I&apos;m a passionate learner fueled by a dedication to advancing my
+            skills in frontend technologies like HTML, CSS, JavaScript, and
+            React.
+          </p>
+        </div>
       </section>
     </div>
   );
