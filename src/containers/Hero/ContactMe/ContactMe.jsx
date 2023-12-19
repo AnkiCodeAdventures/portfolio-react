d3917770e829ca8dd1e0f2e1a03e3163406ad19d
Ankita Agarwal <ankitaag94@gmail.com>
2023-12-19 21:16:26 +0530

    modify contact section

--- src/containers/Hero/ContactMe/ContactMe.jsx
+++ src/containers/Hero/ContactMe/ContactMe.jsx
@@ -9,8 +9,7 @@ function ContactMe() {
         <p className={styles.contactText}>
           Hello there! I'm passionate about continuous learning, exploring
           diverse resources, and bringing new ideas to life through creative
-          projects. Whether you have an exciting opportunity, a collaboration
-          proposal, or simply want to connect, I'd love to hear from you.
+          projects.
         </p>
         <AnchorButton size="lg" link="mailto:ankitaag94@gmail.com">
           Email Me
