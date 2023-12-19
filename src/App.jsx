eb7309c81e5e556234d1b4be887be9fe019b8885
Ankita Agarwal <ankitaag94@gmail.com>
2023-12-19 21:46:31 +0530

    add divider

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import ContactMe from "./containers/Hero/ContactMe/ContactMe";
 import Hero from "./containers/Hero/Hero";
+import Divider from "./components/Divider/Divider";
 
 function App() {
   return (
     <>
       <div>
         <Hero />
+        <Divider />
         <ContactMe />
+        <Divider />
       </div>
     </>
   );
