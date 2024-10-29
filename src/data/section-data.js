ee57c5c4e660d058a2697a6b2b5102b62508cd3d
Ankita <ankitaag94@gmail.com>
2024-10-29 16:23:50 +0530

    fix typo

--- src/data/section-data.js
+++ src/data/section-data.js
@@ -6,7 +6,7 @@ export const sectionData = [
     tagsHeading: "Technologies used include:",
     tags: "React | Mantine UI | JavaScript | CSS Modules | HTML",
     description: {
-      text: "Add your favorite books and explore the mystical land of literature with hobbits, elves, wizards, dragons, gnomes, pixies and many more awaiting you . . ",
+      text: "Add your favorite books and explore the mystical land of literature with hobbits, elves, wizards, dragons, gnomes, pixies and many more awaiting you . . . Explore it",
       href: "https://book-trails.vercel.app/",
       linkText: "here",
     },
